d34245a0684e6dbdd0067e4c317fae672890d6cb
Fabo2303 <fabian666777@gmail.com>
2024-07-24 12:11:10 -0500

    sonarqube analysis

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import sys
+import os
 from datetime import timedelta
 
 from flask import Flask, request, jsonify
@@ -9,14 +10,17 @@ from chatbot.chatbot import recipes, common_message, recommendations
 from config.db import db
 from config.utilz import analysis
 from user.user_controller import users
+from dotenv import load_dotenv
+
+load_dotenv()
 
 print(sys.path)
 app = Flask(__name__)
 
 CORS(app, resources={r"/*": {"origins": "*"}})
 
-app.config['SECRET_KEY'] = '97017582b69247b8a8a88491e108c1ca'
-app.config['JWT_SECRET_KEY'] = '9aa6fe2ac33742958ef600ffea2230fc'
+app.config['SECRET_KEY'] = os.environ["SECRET_KEY"]
+app.config['JWT_SECRET_KEY'] = os.environ["JWT_SECRET_KEY"]
 app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(hours=2)
 app.config['JWT_REFRESH_TOKEN_EXPIRES'] = timedelta(days=1)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///fb_users.sqlite3'
@@ -26,6 +30,8 @@ jwt = JWTManager(app)
 
 db.init_app(app)
 app.register_blueprint(users)
+
+NO_MESSAGE_PROVIDED = {"error": "No message provided"}
 
 
 @app.route('/chat', methods=['POST'])
@@ -40,7 +46,7 @@ def index():
         else:
             return chat_common_message()
     else:
-        return jsonify({"error": "No message provided"}), 400
+        return jsonify(NO_MESSAGE_PROVIDED), 400
 
 
 @app.route('/recipes', methods=['POST'])
@@ -50,7 +56,7 @@ def chat():
         response = recipes(user_input)
         return jsonify({"response": response})
     else:
-        return jsonify({"error": "No message provided"}), 400
+        return jsonify(NO_MESSAGE_PROVIDED), 400
 
 
 @app.route('/recommendations', methods=['POST'])
@@ -60,7 +66,7 @@ def chat_recommendations():
         response = recommendations(user_input)
         return jsonify({"response": response})
     else:
-        return jsonify({"error": "No message provided"}), 400
+        return jsonify(NO_MESSAGE_PROVIDED), 400
 
 
 @app.route('/common_message', methods=['POST'])
@@ -70,11 +76,11 @@ def chat_common_message():
         response = common_message(user_input)
         return jsonify({"response": response})
     else:
-        return jsonify({"error": "No message provided"}), 400
+        return jsonify(NO_MESSAGE_PROVIDED), 400
 
 
 with app.app_context():
     db.create_all()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
